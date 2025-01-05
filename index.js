b5a8665dc5630fdf0c93f5dd1f1cdb52f75ebc02
fyneboy001 <fyneboyfynerose@gmail.com>
2025-01-05 14:09:44 +0100

    My first Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById("darkmode-toggle");
+
+toggleButton.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
+window.alert("Welcome🥰 This is FINEBOY ROSEMARY's Portfolio");
